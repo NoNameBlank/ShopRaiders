7247dc6751da30078dab578cd0c177ce22b54475
Jan und Philipp <Testmail.de>
2022-08-19 15:26:31 +0200

    MainView Phil hinzugefügt

---
+++ src/view/MainView.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class MainView extends JFrame {
+
+	// private StartPanel startPanel;
+
+	public MainView() {
+		super.setBounds(100, 100, 1050, 794);
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		super.setVisible(true);
+	}
+}
