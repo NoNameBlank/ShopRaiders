b461255e77b5053dae5e79f9046cdfacd3754ab2
Philipp <Testmail.de>
2022-08-29 15:33:09 +0200

    kategoriePanel mit Funktion

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionEvent;
 import view.MainView;
 
 public class MainController {
-	
+
 	private MainView mainView;
 
 	public MainView getMainView() {
@@ -15,31 +15,42 @@ public class MainController {
 	public void setMainView(MainView mainView) {
 		this.mainView = mainView;
 	}
-	
+
 	public MainController() {
 		this.setMainView(new MainView());
 		this.getMainView().addActionListenerToStartBtn(this::startBtn);
 		this.getMainView().addActionListenerToBtnWarenkorb(this::showWarenkorbPanel);
-		
+
 		this.getMainView().getMainPanel().getKategoriePanel().getKatList().get(0).addActionListener(this::kat1);
-	
-		
-		
+		this.getMainView().getMainPanel().getKategoriePanel().getKatList().get(1).addActionListener(this::kat2);
+		this.getMainView().getMainPanel().getKategoriePanel().getKatList().get(2).addActionListener(this::kat3);
+
 	}
-	
+
 	private void startBtn(ActionEvent e) {
 		this.getMainView().showMainPanel();
 	}
-	
-	//Btn Warenkorb
+
+	// Btn Warenkorb
 	private void showWarenkorbPanel(ActionEvent e) {
-		System.out.println("AUsgabe: WarenkorbAnzeigen");
+		//System.out.println("AUsgabe: WarenkorbAnzeigen");
 		this.getMainView().showWarenkorbPanel();
 	}
-	
+
 	private void kat1(ActionEvent e) {
-		System.out.println("kat1 wurde gedruckt");
+		this.getMainView().getMainPanel().getCardPanel().getShoppingPanel().showSearchResults("Kat1", "Kat1 ", "Kat1",
+				0);
 	}
-	
+
+	private void kat2(ActionEvent e) {
+		//System.out.println("Kat2 MainController");
+		this.getMainView().getMainPanel().getCardPanel().getShoppingPanel().showSearchResults("Kat2", "Kat2 ", "Kat2",
+				0);
+	}
+
+	private void kat3(ActionEvent e) {
+		this.getMainView().getMainPanel().getCardPanel().getShoppingPanel().showSearchResults("Kat3", "Kat3 ", "Kat3",
+				0);
+	}
 
 }
--- src/view/shopping/ShoppingPanel.java
+++ src/view/shopping/ShoppingPanel.java
@@ -2,6 +2,7 @@ package view.shopping;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JPanel;
@@ -52,9 +53,50 @@ public class ShoppingPanel extends JPanel {
 		this.getsPane().getVerticalScrollBar().setUnitIncrement(16);
 		super.add(this.getsPane());
 
+		this.setArtikelList(new ArrayList<>());
+
 		for (int i = 0; i < 3; i++) {
 			this.getDisplayPanel().add(new ArtikelPanel());
 		}
+
+	}
+
+	public void showSearchResults(String name, String bild, String produktBeschr, double preis) {
+		this.getArtikelList().clear();
+		if (bild.equals("Kat1 ")) {
+			System.out.println("Kat 1 gedrückt ShoppingPanel ausgabe");
+			for (int i = 0; i < 10; i++) {
+				this.getArtikelList()
+						.add(new ArtikelPanel(name + " " + i, bild + " " + i, produktBeschr + " " + i, +preis));
+			}
+
+		} else if (bild.equals("Kat2 ")) {
+			System.out.println("Kat 2 gedrückt ShoppingPanel ausgabe");
+			for (int i = 0; i < 10; i++) {
+				this.getArtikelList()
+						.add(new ArtikelPanel(name + " " + i, bild + " " + i, produktBeschr + " " + i, +preis));
+			}
+		} else if (bild.equals("Kat3 ")) {
+			System.out.println("Kat 3 gedrückt ShoppingPanel ausgabe");
+			for (int i = 0; i < 10; i++) {
+				this.getArtikelList()
+						.add(new ArtikelPanel(name + " " + i, bild + " " + i, produktBeschr + " " + i, +preis));
+			}
+		
+
+		}
+
+		// Alle ArtikelPanel entfernen
+		this.getDisplayPanel().removeAll();
+		// Dem AnzeigePanel ein neues GridLayout geben, da so viele Zeilen benoetigt
+		// werden,
+		// wie Panels in der Liste sind.
+		this.getDisplayPanel().setLayout(new GridLayout(this.getArtikelList().size(), 1));
+		// Dem AnzeigePanel die Artikelpanel hinzufuegen.
+		this.getArtikelList().forEach(item -> this.getDisplayPanel().add(item));
+		// Das Anzeigepanel muss neu gemalt werden.
+		super.revalidate();
+		super.repaint();
 
 	}
 
